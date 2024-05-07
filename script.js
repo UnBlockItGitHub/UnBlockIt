ccf87d7e6c5b819709ae43badf8b380a8e13224b
UnBlockItGitHub <169111520+UnBlockItGitHub@users.noreply.github.com>
2024-05-07 13:24:46 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
 document.getElementById('urlForm').addEventListener('submit', function(event) {
     event.preventDefault();
     var url = document.getElementById('urlInput').value;
-    var archiveUrl = 'https://web.archive.org/save/' + url;
-    document.getElementById('result').innerHTML = 'Fetching archive...';
+    var archiveUrl = 'https://web.archive.org/web/' + url;
 
-    fetch(archiveUrl)
-        .then(response => {
-            if (response.ok) {
-                return response.text();
-            }
-            throw new Error('Network response was not ok.');
-        })
-        .then(data => {
-            document.getElementById('result').innerHTML = '<a href="' + data + '" target="_blank">View Archived Page</a>';
-        })
-        .catch(error => {
-            document.getElementById('result').innerHTML = 'Error: ' + error.message;
-        });
+    document.getElementById('result').innerHTML = 'Getting Unblocked Version Unblocking...';
+
+    document.getElementById('result').innerHTML = '<a href="' + archiveUrl + '" target="_blank">View Archived Page</a>';
 });
